84fecebd3e9adb13423b4700f346ab62a61c059c
Kai Ruhnau <kai.ruhnau@target-sg.com>
2015-02-21 09:15:12 +0100

    Write is now Async. This fixes a race between closing the socket and writing resulting in an ObjectDisposedException that would bring down the loop

--- src/Kestrel/ServerRequest.cs
+++ src/Kestrel/ServerRequest.cs
@@ -235,7 +235,7 @@ namespace Kestrel
                     _frame.ResponseHeaders["Upgrade"] = values;
                 }
             }
-            _frame.ProduceStart();
+            await _frame.ProduceStartAsync();
             return _frame.DuplexStream;
         }
     }
